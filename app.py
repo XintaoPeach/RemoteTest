796b4b0f270f6a08f6a3374da7acc3db597abe36
Xintao <gongzuo_wxt@163.com>
2023-07-10 15:37:21 +0800

    更新app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello Peach')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    print('Hello Peach')
+    return 'Hello Peach'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
