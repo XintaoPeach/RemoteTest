df47b38a09459fdbc2a28c80bb3b6d5ef84dfa76
Xintao <gongzuo_wxt@163.com>
2023-07-28 14:43:07 +0800

    Refresh
    只是简单的更新一下仓库

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # RemoteTest
 远程测试仓库
 
-## 本地修改
-新增txt文件
 > 这只是一个测试仓库
+
+## 主文件
+
+app.py文件中是一个简单的Flask框架，启动以后访问 http://localhost:5000/get_ip 会返回一个IP地址，这个IP地址是你访问该api是的本地地址。
+
+## 联系
+
+GitHub主页：[XintaoPeach (Xintao) (github.com)](https://github.com/XintaoPeach)
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello():
     print('Hello Peach')
     return 'Hello Peach'
+
+
+@app.route('/get_ip')
+def ip():
+    ip = request.remote_addr
+    res = {
+        '用户IP': ip
+    }
+    return res
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
